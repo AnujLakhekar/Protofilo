6c21e3e4295ca67c3eb88721b276ca6699351b14
anuj-Dev56 <anujlakhekar13@gmail.com>
2026-02-14 01:03:47 +0530

    protofilo-v2

--- src/app/v2/_providers/StateProvider.tsx
+++ src/app/v2/_providers/StateProvider.tsx
@@ -1,10 +1,19 @@
 "use client"
 import React, {PropsWithChildren} from 'react'
 
-const StateContext = React.createContext({});
+// Fix context type for state and setState
+interface StateContextType {
+    state: string;
+    setState: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const StateContext = React.createContext<StateContextType>({
+    state: "",
+    setState: () => {},
+});
 
 const StateProvider = ({children}: PropsWithChildren) => {
-    const [state, setState] = React.useState<String>("");
+    const [state, setState] = React.useState<string>("");
     return (
         <StateContext.Provider value={{state, setState}}>{children}</StateContext.Provider>
     )
