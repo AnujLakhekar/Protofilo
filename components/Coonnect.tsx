4d114a7e923c1d9aac1e3d701aae0d6e875dd08b
Anuj Vinod Lakhekar <anujlakhekar4@jdcoem.ac.in>
2026-09-11 20:27:23 +0530

    Fix type definition for renderStepIndicator prop

--- components/Coonnect.tsx
+++ components/Coonnect.tsx
@@ -10,7 +10,7 @@ const Coonnect = () => {
         <div className="w-full max-w-lg mx-auto px-2 sm:px-4 md:px-8">
             <Stepper
   initialStep={0}
-  renderStepIndicator={(props) => <div>Step {props.currentStep}</div>}
+  renderStepIndicator={(props: any) => <div>Step {props.currentStep}</div>}
   onStepChange={() => console.log("Step changed")}
   onFinalStepCompleted={() => {
     console.log("Form Data:", { name, email, message });
